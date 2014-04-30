b826b6065992e1ac320df0f3f378d239d12b2ea2
DooMLoRD <metoo.mememe@gmail.com>
2014-04-30 21:46:21 +0530

    kernel: Move REPEAT_BYTE definition into linux/kernel.h (part 1)
    Conflicts:
    	arch/x86/include/asm/word-at-a-time.h

--- arch/x86/include/asm/word-at-a-time.h
+++ arch/x86/include/asm/word-at-a-time.h
@@ -2,6 +2,17 @@
 #define _ASM_WORD_AT_A_TIME_H
 
 
+#include <linux/kernel.h>
+
+/*
+ * This is largely generic for little-endian machines, but the
+ * optimal byte mask counting is probably going to be something
+ * that is architecture-specific. If you have a reliably fast
+ * bit count instruction, that might be better than the multiply
+ * and shift, for example.
+ */
+
+
 #ifdef CONFIG_64BIT
 
 static inline long count_masked_bytes(unsigned long mask)
@@ -21,8 +32,8 @@ static inline long count_masked_bytes(long mask)
 
 #endif
 
-#define REPEAT_BYTE(x)	((~0ul / 0xff) * (x))
 
+/* Return the high bit set in the first byte that is a zero */
 static inline unsigned long has_zero(unsigned long a)
 {
 	return ((a - REPEAT_BYTE(0x01)) & ~a) & REPEAT_BYTE(0x80);
