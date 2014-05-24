26579edeb1aea4707bc1c79ddaf44970044b59a8
flar2 <asegaert@gmail.com>
2014-05-23 22:20:07 -0400

    GCC 4.9.1: fix compile warnings
    Signed-off-by: flar2 <asegaert@gmail.com>
    
    Conflicts:
    	kernel/workqueue.c
    	net/ipv4/tcp_input.c
    	net/unix/af_unix.c
    
    Change-Id: I0f87872ae1324e4ff33b0a867b765324799e5fab

--- drivers/power/qpnp-bms.c
+++ drivers/power/qpnp-bms.c
@@ -1796,7 +1796,7 @@ static int get_prop_bms_current_now(struct qpnp_bms_chip *chip)
 
 static int get_prop_bms_charge_counter(struct qpnp_bms_chip *chip)
 {
-	int64_t cc_raw;
+	int64_t cc_raw = 0;
 
 	mutex_lock(&chip->bms_output_lock);
 	lock_output_data(chip);
@@ -1809,7 +1809,7 @@ static int get_prop_bms_charge_counter(struct qpnp_bms_chip *chip)
 
 static int get_prop_bms_charge_counter_shadow(struct qpnp_bms_chip *chip)
 {
-	int64_t cc_raw;
+	int64_t cc_raw = 0;
 
 	mutex_lock(&chip->bms_output_lock);
 	lock_output_data(chip);
@@ -2435,7 +2435,7 @@ out:
 #endif 
 static int clamp_soc_based_on_voltage(struct qpnp_bms_chip *chip, int soc)
 {
-	int rc, vbat_uv, batt_temp;
+	int rc, vbat_uv = 0, batt_temp;
 
 	rc = get_battery_voltage(chip, &vbat_uv);
 	if (rc < 0) {
@@ -2717,7 +2717,7 @@ done_calculating:
 static int calculate_soc_from_voltage(struct qpnp_bms_chip *chip)
 {
 	int voltage_range_uv, voltage_remaining_uv, voltage_based_soc;
-	int rc, vbat_uv;
+	int rc, vbat_uv = 0;
 
 	rc = get_battery_voltage(chip, &vbat_uv);
 	if (rc < 0) {
@@ -2963,7 +2963,7 @@ static void configure_vbat_monitor_high(struct qpnp_bms_chip *chip)
 static void btm_notify_vbat(enum qpnp_tm_state state, void *ctx)
 {
 	struct qpnp_bms_chip *chip = ctx;
-	int vbat_uv;
+	int vbat_uv = 0;
 	struct qpnp_vadc_result result;
 	int rc;
 
@@ -4192,7 +4192,7 @@ static void disable_ocv_update_with_reason(bool disable, int reason)
 static void pm8941_btm_voltage_alarm_notify(enum qpnp_tm_state state, void *ctx)
 {
 	struct qpnp_bms_chip *chip = ctx;
-	int vbat_uv;
+	int vbat_uv = 0;
 	struct qpnp_vadc_result result;
 
 	qpnp_vadc_read(chip->vadc_dev, VBAT_SNS, &result);
