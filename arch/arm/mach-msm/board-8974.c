fe7b5c8a835d5d0868dda5ffcfec433a64116e6c
Rodolfo Lopez <maxrdlf95@gmail.com>
2014-06-01 02:14:06 -0430

    Fix intelli/msm_thermal for HTC M8

--- arch/arm/mach-msm/board-8974.c
+++ arch/arm/mach-msm/board-8974.c
@@ -99,10 +99,8 @@ void __init msm8974_add_drivers(void)
 	else
 		msm_clock_init(&msm8974_clock_init_data);
 	tsens_tm_init_driver();
-	#ifdef CONFIG_INTELLI_THERMAL
-	msm_thermal_init(NULL);
-#else
- 	msm_thermal_device_init();
+#ifdef CONFIG_INTELLI_THERMAL
+	msm_thermal_device_init();
 #endif
 }
 
