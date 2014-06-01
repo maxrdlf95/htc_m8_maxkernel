4d097369573c56073718f0628c19a067e4207a02
Rodolfo Lopez <maxrdlf95@gmail.com>
2014-06-01 13:32:10 -0430

    msm: disable CONFIG_MSM_DCVS and all dependent code from maxwen

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -119,8 +119,10 @@ extern unsigned long avg_nr_running(void);
 extern unsigned long nr_iowait_cpu(int cpu);
 extern unsigned long this_cpu_load(void);
 
+#if defined(CONFIG_MSM_DCVS)
 extern void sched_update_nr_prod(int cpu, unsigned long nr, bool inc);
 extern void sched_get_nr_running_avg(int *avg, int *iowait_avg);
+#endif
 
 extern void calc_global_load(unsigned long ticks);
 
