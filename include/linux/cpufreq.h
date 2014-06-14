67f27661b65929089001bfc2f5f1f49cc7dc3e7a
Rodolfo Lopez <maxrdlf95@gmail.com>
2014-06-13 20:08:58 -0430

    Revert "Revert "Revert "New intellidemand governor by @faux123"""
    This reverts commit dd88a59c0a02d810dfb70b1c3164534cf1357c33.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -337,9 +337,6 @@ extern struct cpufreq_governor cpufreq_gov_lionheart;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LAGFREE)
 extern struct cpufreq_governor cpufreq_gov_lagfree;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_lagfree)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIDEMAND)
-extern struct cpufreq_governor cpufreq_gov_intellidemand;
-#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_intellidemand)
 
 #endif
 
